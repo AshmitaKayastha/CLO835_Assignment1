becff08a47326ba1792ccf7a701fab3538bcffeb
AshmitaKayastha <ashmee5672gmail.com>
2025-06-10 15:12:23 +0000

    sql script updated

--- mysql.sql
+++ mysql.sql
@@ -16,6 +16,8 @@ INSERT INTO employee VALUES ('5','Ala','Will','Engineer','Canada');
 INSERT INTO employee VALUES ('6','Elon','Musk','Engineer','USA');
 INSERT INTO employee VALUES ('7', 'Bob', 'Liam', 'Doctor', 'China');
 INSERT INTo employee VALUES ('8', 'liam', 'Bob', 'Doctor', 'Canada');
+INSERT INTo employee VALUES ('9', 'John', 'Bob', 'IT Support', 'Canada');
+
 
 SELECT * FROM employee;
 
